85b4dffe8ab9532420bc6738fd8a3ebd76256e79
Maks <carcillarmax@gmail.com>
2024-01-08 17:10:36 +0800

    if-else statements

--- greatest_number_finder.py
+++ greatest_number_finder.py
@@ -1,22 +1,31 @@
-#Ask  user for 1st number input
-num_one = int(input("Give me the first number: "))
+# Ask user for the 1st number input
+num_one = int(input("Give me a number: "))
 
-#Ask user for 2nd number input
+# Ask user for the 2nd number input
 num_two = int(input("Give me the second number: "))
 
-#Ask user for 3rd number input
+# Ask user for the 3rd number input
 num_three = int(input("Give me the third number: "))
 
-#Check if the 1st number is greater than both the 2nd and 3rd numbers
-#If true, print 1st number
+# Check if the 1st number is greater than both the 2nd and 3rd numbers
+if num_one > num_two and num_one > num_three:
+    # If true, print 1st number
+    print(num_one)
 
-#Else:
-    #Check if the 2nd number is greater than both the 1st and 3rd numbers
-    #If true, print 2nd number
-
-    #Else:
-        #Check if the 3rd number is greater than both the 1st and 2nd numbers
-        #if true, print 3rd number
+# If false, go to the next condition
+else:
+    # Check if the 2nd number is greater than both the 1st and 3rd numbers
+    if num_two > num_one and num_two > num_three:
+        # If true, print 2nd number
+        print(num_two)
     
-        #Else:
-        #print all equal
+    # If both of the conditions are false, go to the next condition
+    else:
+        # Check if the 3rd number is greater than both the 1st and 2nd numbers
+        if num_three > num_one and num_three > num_two:
+            # If true, print 3rd number
+            print(num_three)
+
+        # If all conditions are false, print that they're all equal
+        else:
+            print("They're all equal!")
