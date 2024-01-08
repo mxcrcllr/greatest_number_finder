5e6193225a417ac44fcf7a82f4cbeab945ce6597
Maks <carcillarmax@gmail.com>
2024-01-08 16:03:56 +0800

    ask user to input three numbers

--- greatest_number_finder.py
+++ greatest_number_finder.py
@@ -1,8 +1,11 @@
 #Ask  user for 1st number input
+num_one = int(input("Give me the first number: "))
 
 #Ask user for 2nd number input
+num_two = int(input("Give me the second number: "))
 
 #Ask user for 3rd number input
+num_three = int(input("Give me the third number: "))
 
 #Check if the 1st number is greater than both the 2nd and 3rd numbers
 #If true, print 1st number
