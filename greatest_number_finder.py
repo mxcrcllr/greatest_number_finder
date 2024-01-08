a5ed146084edc805844095f60fd373bad19ca2cd
Maks <carcillarmax@gmail.com>
2024-01-08 15:54:04 +0800

    pseudocode

---
+++ greatest_number_finder.py
@@ -0,0 +1,19 @@
+#Ask  user for 1st number input
+
+#Ask user for 2nd number input
+
+#Ask user for 3rd number input
+
+#Check if the 1st number is greater than both the 2nd and 3rd numbers
+#If true, print 1st number
+
+#Else:
+    #Check if the 2nd number is greater than both the 1st and 3rd numbers
+    #If true, print 2nd number
+
+    #Else:
+        #Check if the 3rd number is greater than both the 1st and 2nd numbers
+        #if true, print 3rd number
+    
+        #Else:
+        #print all equal
